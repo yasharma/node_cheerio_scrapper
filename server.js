e0c3a3be44c50517e55799622c39656c21640a17
yasharma <yashsharma205@gmail.com>
2017-03-21 20:46:39 +0530

    Update

--- server.js
+++ server.js
@@ -7,18 +7,20 @@ app 		= express(),
 request 	= require('request'),
 fs 			= require('fs'),
 url 		= require("url"),
+querystring = require('querystring'),
 PORT 		= process.env.PORT ||5000;
 
 app.get('/scrap', function (req, res, next) {
-	let results = [], scrapper = {}, scrapUrl = 'http://www.bing.com/search?q=';
+	let results = [], scrapper = {}, scrapUrl = 'https://www.bing.com/search?q=';
 	if( !req.query.q ){
 		return res.json({success: false, message: 'No query found'});
 	}
 	let query = `${scrapUrl}${req.query.q}`, finalUrl ,
-	first = (req.query.first) ? req.query.first : 0; 
+	first = (req.query.first > 1) ? req.query.first : 0; 
 	
 	if( first != 0 && first != 1){
-		finalUrl = `${query}&first=${first}`;
+		let _querystring = querystring.stringify (req.query);
+		finalUrl = `https://www.bing.com/search?${_querystring}`;
 	} else {
 		finalUrl = query;
 	}
@@ -36,7 +38,7 @@ app.get('/scrap', function (req, res, next) {
 			results.push({title: title, description: description, url: url});
 		});
 		scrapper = {
-			next: queryParams,
+			next: nextUrl,
 			result: results
 		}
 		res.json(scrapper);
